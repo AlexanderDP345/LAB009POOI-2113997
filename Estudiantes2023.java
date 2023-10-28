376d60717e5e93a2ddac2ae5e55a736dac7b0cc7
༻ 4lex ༺ <84357529+AlexanderDP345@users.noreply.github.com>
2023-10-27 23:10:18 -0500

    Add files via upload

---
+++ Estudiantes2023.java
@@ -0,0 +1,76 @@
+import java.util.*;
+import javax.swing.*;
+import java.io.*;
+
+public class Estudiantes2023 {
+    private static ArrayList<Estudiante> estudiantes;
+
+    public Estudiantes2023() {
+        estudiantes = new ArrayList<>();
+        cargarArchivo();
+    }
+
+    public void adicionar(Estudiante estudiante) {
+        //Introduce código
+    }
+
+    public void eliminar(Estudiante estudiante) {
+        //Introduce código
+    }
+
+    public Estudiante obtener(int pos) {
+	//Introduce código
+        return null;
+    }
+
+    public Estudiante buscar(int codigo) {
+	//Introduce código        
+	  return null;
+    }
+
+    public int tamano() {
+        //Introduce código        
+	  return 0;
+    }
+
+    public void listarEstudiantes(){
+        //Introduce código        
+    }
+
+    // Métodos para manipular el archivo de texto
+    private void readFromInputStream(InputStream inputStream) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        String linea;
+        while ((linea = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(linea, ",");
+            createStudent(st);
+        }
+        br.close();
+    }
+
+	// Método que añadir a los estudiantes del archivo .txt en el arrayList estudiantes.
+    private void createStudent(StringTokenizer st){
+        int codigo = Integer.parseInt(st.nextToken().trim());
+        String nombre = st.nextToken().trim();
+        int ciclo = Integer.parseInt(st.nextToken().trim());
+        double pension = Double.parseDouble(st.nextToken().trim());
+        Estudiante estudiante = new Estudiante(codigo, nombre, ciclo, pension);
+        adicionar(estudiante);
+    }
+
+    // Métodos para manipular el archivo de texto
+    private void cargarArchivo() {
+        try {
+            File file = new File("./src/estudiantes.txt");
+            Class<Estudiantes2023> clazz = Estudiantes2023.class;
+            InputStream inputStream = clazz.getResourceAsStream("/estudiantes.txt");
+            if (file.exists()) {
+                readFromInputStream(inputStream);
+            } else
+                JOptionPane.showMessageDialog(null,
+                        "El archivo estudiantes.txt no existe");
+        } catch (Exception x) {
+            JOptionPane.showMessageDialog(null, "Se produjo un error= " + x);
+        }
+    }
+}
